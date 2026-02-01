9a44f90449e0d29162d72e57e30ead25477f45e5
rakib <rakibco66@gmail.com>
2026-02-01 20:58:55 +0600

    learn array index

---
+++ index.js
@@ -0,0 +1,11 @@
+const numbers = [13, 44, 56, 23, 77, 20]
+
+numbers[2] =  4;
+console.log(numbers)
+
+let abc = 45;
+abc = 88;
+
+numbers[1] = 9999
+
+console.log(numbers)
---
+++ length.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 55, 734, 77, 43, 77, 4];
+
+
+console.log(numbers.length)
